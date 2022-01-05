90c7e5f21801ef235638f706904d29de44a75162
Eric L. Hepperle <codewizard13@gmail.com>
2022-01-05 13:44:53 -0600

    WEBPACK IS DYNAMICALLY UPDATING BUNDLE.JS ON SAVE

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,8 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.css$/i
+                test: /\.css$/i,
+                use: ['style-loader','css-loader']
             }
         ]
     }
