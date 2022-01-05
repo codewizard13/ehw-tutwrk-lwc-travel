5fcf1f103afe52193f368659272a53a02569507d
Eric L. Hepperle <codewizard13@gmail.com>
2022-01-05 10:24:56 -0600

    WEBPACK WORKING - CH 13 - 1640

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,10 @@
+const path = require('path')
+
 module.exports = {
-    entry: './app/assets/scripts/App.js'
+    entry: './app/assets/scripts/App.js',
+    output: {
+        filename: 'bundled.js',
+        path: path.resolve(__dirname, 'app')
+    },
+    mode: 'development'
 }
