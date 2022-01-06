ebae54988553dc5f26a67ca25b183058731e6929
Eric L. Hepperle <codewizard13@gmail.com>
2022-01-06 08:33:13 -0600

    CH 17 - 1000 - POSTCSS HANDLING IMPORTS

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 
 const postCSSPlugins = [
+    require('postcss-import'),
     require('postcss-simple-vars'),
     require('postcss-nested'),
     require('autoprefixer')
